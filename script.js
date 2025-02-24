a80396810459537eac676d97a381ac2e5e0a43b7
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-24 14:30:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let tim= document.getElementById("timer");
+let currDate = new Date();
+
+setInterval(function () {
+	currDate = new Date();
+	tim.innerHTML= currDate.toLocaleString();
+},1000)
+
